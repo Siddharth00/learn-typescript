dda1b0fb9a9465333694331dffb323220668fe10
Siddharth00 <sidsid345@gmail.com>
2023-02-18 18:48:52 +0530

    tuples

---
+++ learn/06-tuples/tuples.ts
@@ -0,0 +1,26 @@
+type ThreeDCoordinate = [x: number, y: number, z: number];
+
+function add3DCoordinate(c1: ThreeDCoordinate, c2: ThreeDCoordinate): ThreeDCoordinate {
+    return [
+        c1[0] + c2[0],
+        c1[1] + c2[1],
+        c1[2] + c2[2],
+    ]
+}
+
+console.log(add3DCoordinate([1, 2, 3], [4, 5, 6]));
+
+function useState(initial: string): [() => string, (v: string) => void] {
+    let str: string = initial;
+    return [
+        () => str,
+        (v: string) => {
+            str = v;
+        }
+    ]
+}
+
+const [myName, setMyName] = useState("Sid");
+console.log(myName());
+setMyName("Siddharth");
+console.log(myName());
